9c2168ebd1edbd6235697d8143795fb9b2d6519c
Wassim Slimane <wassim.slimane01@gmail.com>
2024-01-12 22:53:42 -0600

    feat(main.js): Create new objects inherited from classes to playand learn the power of Class in js

--- main.js
+++ main.js
@@ -1,6 +1,38 @@
 import User from "./classes/User.js";
 import Article from "./classes/Article.js";
 
-() => {
-    console.log('hehehe');
+const main = () => {
+    const user = new User("wslimane21@gmail.com", "password", "wassim", 23);
+    const article = new Article("book", 34, "");
+    
+    console.log(typeof user);
+    console.log(typeof article);
+
+    console.log(user);
+    console.log(article);
+
+    console.log(Object.isSealed(user));
+    
+    user.name = "slimane"; 
+    console.log(user);
+
+    // Getters
+    console.log(user.email);
+    console.log(user.getEmail);
+    console.log(user.age);
+    console.log(user.getAge);
+
+    // Setters
+    user.setAge = 24
+    console.log(user.getAge);
+    user.age = 32;
+    console.log(user.age);
+
+    // Static
+    User.setDateCreation();
+    console.log(User.dateCreation);
+    console.log(user.dateCreation);
 }
+
+main();
+

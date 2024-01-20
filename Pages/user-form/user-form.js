dcaf92519bce8bf59ee86e23ef9a954dc1c04f9e
Wassim Slimane <wassim.slimane01@gmail.com>
2024-01-20 09:07:28 -0600

    refacto(user-form): storage function modified and json-server added to have an api with endpoint

--- Pages/user-form/user-form.js
+++ Pages/user-form/user-form.js
@@ -1,48 +1,35 @@
 import User from "../../Models/User.js";
 
 const handleUserForm = {
+    apiUrl: "http://localhost:3000/users",
+    optionsPost: {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    },
+
     validator(data) {
         return data ? true : false;
     },
+
     storeData(data) {
         const user = new User(data.email, data.password, data.name, data.age);
-        console.log(user);
-        fetch('../../db/live.json')
-        .then((response) => response.json())
-        .then((data) => {
-            // Add the user to db
-            data.users = [...data.users, user];
-            JSON.stringify(data);
-        })
-        .then((jsonText) => {
-            // Create a Blob with the updated data
-            const updatedDataBlob = new Blob([jsonText], { type: 'application/json' });
-
-            // Create a File from the Blob
-            const updatedDataFile = new File([updatedDataBlob], 'live.json', { type: 'application/json' });
-
-            // Save the updated file
-            this.saveAs(updatedDataFile);
-        })
-        .catch((error) => console.log(error));
-    },
-
-    saveAs(blob, fileName) {
-        const link = document.createElement('a');
-        link.href = URL.createObjectURL(blob);
-        link.download = fileName;
-        link.click();
+        this.optionsPost.body = JSON.stringify(user);
+        
+        return fetch(this.apiUrl, this.optionsPost);
     }
 }
 
 export default handleUserForm;
 
 // Faire le traitement des données (required, email password) => send error message
-// new User(data)
-// store user in live-json
 
-// Reprendre la théorie
 
 // Voir si c'est possible de mettre en place des tests (avec un outil)
 
 // Ajouter tailwind pour le css
+
+// Faire un fichier .env avec les bases_url 
+// Faire un fichier ./services/userRequests.js => function userRequests() => avec toutes les fonctions CRUD pour le user
+// Faire un fichier à part pour le validator avec la gestion des formulaires
